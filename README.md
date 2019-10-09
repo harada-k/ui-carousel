64391774e762afa07b98fac6432bb5404602b629
harada-k <harada-k@gnavi.co.jp>
2019-10-09 17:26:23 +0900

    スワイプアクションへの対応

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ CodeGridの記事を元に、カルーセルUIを作成
 - https://app.codegrid.net/entry/2018-common-ui-11
 - https://app.codegrid.net/entry/2018-common-ui-12
 - https://app.codegrid.net/entry/2018-common-ui-13
+- https://app.codegrid.net/entry/2018-common-ui-14
+- https://app.codegrid.net/entry/2018-common-ui-15
+- https://app.codegrid.net/entry/2018-common-ui-16
 
 カルーセルUIにおけるJavaScriptの基本的な考え方を学ぶ。
 
@@ -23,6 +26,14 @@ js
 - タッチ動作への対応にはTouch EventsとPointer Eventsの両方に対応する必要がある
 - タッチ周りのイベントは`.Carousel-stage`で処理
     - `.Carousel-content`と同じサイズであり、移動することもない`.Carousel-stage`を対象とする
+- タッチ以外（ペン、マウス）でもスワイプ動作を行う場合（第16回）、ポインタの種類の判定処理を削除するが、以下の不具合を調整する必要がある。
+    1. スワイプ中にパネル外までカーソルを動かすと動作がおかしくなる
+        - パネル外にカーソルが移動したらスワイプ処理を終了したと扱う（pointerleaveイベントを使用）
+    2. スワイプ終了時（pointerup時）に、リンク先に移動してしまう
+        - pointerdown時とpointerup時のポインタの位置を比較し、ポインタが移動していない場合はクリックとして扱うことで対処
 
 ### 課題
-
+- jsの見通しをよくする。
+- 機能を「メイン」と「オプション」に分け、柔軟に利用できるようにする。
+- 「最後のパネルから最初のパネルに戻るときは次のパネルとして扱う」パターンを実装してみる。
+- クロスフェードのパターンを実装してみる。
--- script.js
+++ script.js
@@ -18,6 +18,9 @@
   // ボタン作成
   const prevButton = createButton(['Carousel-button', '_prev'], '前のパネルを表示する');
   const nextButton = createButton(['Carousel-button', '_next'], '次のパネルを表示する');
+  // 再生ボタン
+  const playButton = createButton(["Carousel-playButton"], "自動再生を開始する");
+  const pauseButton = createButton(["Carousel-pauseButton"], "自動再生を停止する");
   // ボタンが非活性かどうかを格納する変数
   // 状態をキャッシュしておくことで、判定の際にDOMにアクセスする必要がなくなる
   const controllersDisabledStatus = {
@@ -31,11 +34,18 @@
   // タッチのスタートX地点、X軸の移動距離、`.Carousel-stage`内でタッチがスタートしたかを管理
   const pointer = {
     startX: 0,
+    startY: 0,
     moveX: 0,
+    moveY: 0,
     hold: false,
+    click: false
   };
   // slideAnim関数で指定しているパネルがぴたりと合う状態をキャッシュし、追従動作に使用
   let currentTransformValue = '';
+  // 自動再生
+  const autoInterval = 3000;
+  let autoPlay = true;
+  let timeoutId = null;
 
   function createButton(classNames, text) {
     const button = document.createElement('button');
@@ -82,10 +92,19 @@
   // 作成したボタンをDOMに挿入
   function appendNavigations() {
     const controllersFragment = document.createDocumentFragment();
+    const footerFragment = document.createDocumentFragment();
     controllersFragment.appendChild(prevButton);
     controllersFragment.appendChild(nextButton);
     container.appendChild(controllersFragment);
-    footer.appendChild(indicator);
+    footerFragment.appendChild(playButton);
+    footerFragment.appendChild(pauseButton);
+    footerFragment.appendChild(indicator);
+    if (autoPlay) {
+      playButton.disabled = true;
+    } else {
+      pauseButton.disabled = true;
+    }
+    footer.appendChild(footerFragment);
   }
 
   // ボタン押下時の動作
@@ -156,6 +175,10 @@
     updateIndicatorDisabledProp(index);
     // 現在アクティブなインデックスを書き換え
     currentIndex = index;
+    // autoPlay
+    if (autoPlay) {
+      autoNext();
+    }
   }
 
   // パネル表示を変更
@@ -169,49 +192,86 @@
   }
 
   // タッチされたときのイベントハンドラ
+  function pointerDown(clientX, clientY) {
+    // pointer変数にスタート時点の状況を保存していく
+    pointer.startX = clientX;
+    pointer.startY = clientY;
+    pointer.hold = true;
+    // transition-durationの値が大きい（現在は0.2s）ままだと追従時に違和感のある動作となってしまうので
+    // transition-durationの値を0sに変更（タッチが終了した時点で削除）
+    content.style.transitionDuration = '0s';
+    // 一時停止
+    pause();
+  }
+
+  // touchイベントの場合
   function onTouchstart(event) {
     // タッチのポイントが複数かどうかを判定
     // ピンチイン/アウトなどマルチタッチ動作の場合は何もしない
     if (event.targetTouches.length > 1) {
       return
     }
-    // pointer変数にスタート時点の状況を保存していく
-    pointer.startX = event.targetTouches[0].clientX;
-    pointer.hold = true;
-    // transition-durationの値が大きい（現在は0.2s）ままだと追従時に違和感のある動作となってしまうので
-    // transition-durationの値を0sに変更（タッチが終了した時点で削除）
-    content.style.transitionDuration = '0s';
+    const targetTouch = event.targetTouches[0];
+    pointerDown(targetTouch.clientX, targetTouch.clientY);
+  }
+
+  // Pointer Eventsの場合
+  function onPointerdown(event) {
+    // event.pointerType !== 'touch' ：ポインタの種類がタッチかどうかを判定（マウスやペンではスワイプ動作を行えない）
+    // event.isPrimary ：現在のポインタが一番主要なものかどうか、タッチの場合は一番最初にタッチした指かどうかを判定（2本目以降の指がタッチされて動作が不安定になるのを防ぐ）
+    // if (event.pointerType !== 'touch' || !event.isPrimary) {
+    // タッチ以外でもスワイプ動作を行う場合
+    // event.buttons !== 1 ：メインボタンの判定処理
+    // 値1はマウスのメインボタン
+    if (!event.isPrimary || event.buttons !== 1) {
+      return;
+    }
+    event.preventDefault();
+    pointerDown(event.clientX, event.clientY);
   }
 
   // タッチした指が移動した際の処理（追従動作）
   // 現在タッチしているX軸の位置とスタート時のX軸の位置を比較し、その差をcontentのスタイルに反映
+  function pointerMove(clientX, clientY) {
+    // 現在タッチしているX軸の位置 - スタート時のX軸の位置
+    const moveX = clientX - pointer.startX;
+    const additionalTransformValue = 'translateX(' + moveX + 'px)';
+    // pointer.moveXに現在位置を保存
+    pointer.moveX = moveX;
+    // Y軸の移動距離の保持
+    pointer.moveY = clientY - pointer.startY;
+    // キャッシュしておいたtransformの値と、タッチで動かした分のtransformの値をcontentのスタイルに反映
+    // transformプロパティの値には数種類の関数を空白区切りで指定可能（同じ関数を複数回使っても問題なし）
+    content.style.transform = currentTransformValue + ' ' + additionalTransformValue;
+  }
+
+  // タッチイベントの場合
   function onTouchmove(event) {
     if (event.targetTouches.length > 1 || !pointer.hold) {
       return;
     }
-    // 現在タッチしているX軸の位置
-    const currentX = event.targetTouches[0].clientX;
-    // 現在タッチしているX軸の位置 - スタート時のX軸の位置
-    const moveX = currentX - pointer.startX;
-    const additionalTransformValue = 'translateX(' + moveX + 'px)';
-    // キャッシュしておいたtransformの値と、タッチで動かした分のtransformの値をcontentのスタイルに反映
-    // transformプロパティの値には数種類の関数を空白区切りで指定可能（同じ関数を複数回使っても問題なし）
-    content.style.transform = currentTransformValue + ' ' + additionalTransformValue;
-    // pointer.moveXに現在位置を保存
-    pointer.moveX = moveX;
+    // event.targetTouches[0].clientX ：現在タッチしているX軸の位置
+    const targetTouch = event.targetTouches[0];
+    pointerMove(targetTouch.clientX, targetTouch.clientY);
+  }
+
+  // pointerイベントの場合
+  function onPointermove(event) {
+    // if (event.pointerType !== 'touch' || !event.isPrimary || !pointer.hold) {
+    // タッチ以外でもスワイプ動作を行う場合
+    if (!event.isPrimary || event.buttons !== 1 || !pointer.hold) {
+      return;
+    }
+    event.preventDefault();
+    pointerMove(event.clientX, event.clientY);
   }
 
   // タッチ終了時の処理
-  function onTouchend(event) {
-    if (event.targetTouches.length > 1 || !pointer.hold) {
-      return;
-    }
-
+  function pointerUp() {
     // タッチが終了した時点で次にどのインデックスのパネルが表示されるべきか、という処理の結果のインデックス
     const nextIndex = getIndexSwipeEnd();
     // スタート時に指定したスタイルtransition-durationを削除し、通常どおりのトランジションが行われるようにする
     content.style.transitionDuration = '';
-
     // パネルを変更しないならキャッシュしておいたtransformの値を指定し直す
     if (nextIndex === currentIndex) {
       content.style.transform = currentTransformValue;
@@ -220,9 +280,18 @@
       changeItem(nextIndex)
     }
     // pointerオブジェクトの各プロパティ値を初期化
-    pointer.startX = 0;
-    pointer.moveX = 0;
-    pointer.hold = false;
+    // pointer.startX = 0;
+    // pointer.moveX = 0;
+    // pointer.hold = false;
+    // ポインタの移動距離を判定し、X軸とY軸のどちらもゼロならばクリックであると判定し、pointer.clickにtrueを格納
+    if (!pointer.moveX && !pointer.moveY) {
+      pointer.click = true;
+    } else {
+      // そうでなければスワイプと判断し、pointerオブジェクトをリセット
+      resetPointerObj();
+    }
+    // 再生
+    play();
 
     function getIndexSwipeEnd() {
       // X軸移動距離の絶対値を格納（正の符号を持つ）
@@ -249,20 +318,151 @@
     }
   }
 
+  // タッチイベントの場合
+  function onTouchend(event) {
+    if (event.targetTouches.length > 1 || !pointer.hold) {
+      return;
+    }
+    pointerUp();
+  }
+
+  // pointerイベントの場合
+  function onPointerup(event) {
+    // if (event.pointerType !== 'touch' || !event.isPrimary || !pointer.hold) {
+    // タッチ以外でもスワイプ動作を行う場合
+    if (!event.isPrimary || !pointer.hold) {
+      return;
+    }
+    event.preventDefault();
+    pointerUp();
+  }
+
+  // クリック時のイベントハンドラを追加
+  function onClick(event) {
+    // クリックと判定されていなければ、つまり、スワイプと判断されていればデフォルトの動作（この場合はリンクによる遷移）を無効化
+    if (!pointer.click) {
+      event.preventDefault();
+      return;
+    }
+    // クリックと判定されていれば、pointerオブジェクトをリセット
+    resetPointerObj();
+  }
+
+  // pointerオブジェクトをリセットする関数を追加した
+  function resetPointerObj() {
+    pointer.startX = 0;
+    pointer.startY = 0;
+    pointer.moveX = 0;
+    pointer.moveY = 0;
+    pointer.hold = false;
+    pointer.click = false;
+  }
+
+  // スワイプとタッチ動作の衝突の解決
+  // ブラウザのタッチ規定動作を無効化
+  function addTouchActionNone() {
+    stage.style.touchAction = 'none';
+  }
+
+  // 一時停止処理
+  // pointerenterとmouseenterの場合の処理
+  function onPointerenter(event) {
+    // Pointer Events環境用。イベントがタッチであれば何もしない
+    if (event.pointerType === 'touch') {
+      return;
+    }
+    // タッチ以外であればpuase関数実行
+    pause();
+  }
+
+  // 再生処理
+  // pointerleaveとmouseleaveの場合の処理
+  function onPointerleave(event) {
+    // Pointer Events環境用。イベントがタッチであれば何もしない
+    if (event.pointerType === 'touch') {
+      return;
+    }
+    // タッチ以外であればplay関数実行
+    play();
+  }
+
+  // 自動再生
+  function autoNext() {
+    // タイマーがあったら破棄
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    // 再度タイマーをスタート
+    timeoutId = setTimeout(function () {
+      const nextIndex = currentIndex + 1;
+      const targetIndex = nextIndex > lastIndex ? 0 : nextIndex;
+      changeItem(targetIndex);
+    }, autoInterval)
+  }
+
+  // 再生
+  function play() {
+    autoPlay = true;
+    autoNext();
+  }
+
+  // 一時停止
+  function pause() {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    autoPlay = false
+  }
+
+  // 再生ボタン
+  function onClickPlayButton(event) {
+    playButton.disabled = true;
+    pauseButton.disabled = false;
+    play();
+  }
+
+  // 一時停止ボタン
+  function onClickPauseButton(event) {
+    pauseButton.disabled = true;
+    playButton.disabled = false;
+    pause();
+  }
+
   // 初期化
   function init() {
     // ボタンのイベント
     prevButton.addEventListener('click', onClickPrevButton, false);
     nextButton.addEventListener('click', onClickNextButton, false);
+    playButton.addEventListener('click', onClickPlayButton, false);
+    pauseButton.addEventListener('click', onClickPauseButton, false);
     // インジケータボタンのクリック
     // 数が多いので、イベントバブリングを使ってインジケータのルートとなるindicatorにイベントハンドラを追加
     indicator.addEventListener('click', onClickIndicatorButton, false);
-    // タッチイベント
-    stage.addEventListener('touchstart', onTouchstart, false);
-    stage.addEventListener('touchmove', onTouchmove, false);
-    stage.addEventListener('touchend', onTouchend, false);
-    // 何らかの原因でタッチが中断されたときにもタッチ終了時と同じ動作を実行
-    stage.addEventListener('touchcancel', onTouchend, false);
+    // Pointer Eventsをサポートしているかどうかを判定し、イベントリスナを追加
+    if ('PointerEvent' in window) {
+      // ブラウザのタッチ規定動作を無効化
+      addTouchActionNone();
+      stage.addEventListener('pointerdown', onPointerdown, false);
+      stage.addEventListener('pointermove', onPointermove, false);
+      stage.addEventListener('pointerup', onPointerup, false);
+      stage.addEventListener('pointercancel', onPointerup, false);
+      stage.addEventListener('pointerenter', onPointerenter, false);
+      stage.addEventListener('pointerleave', onPointerleave, false);
+      // タッチ以外でもスワイプ動作を行う場合
+      // パネル外にカーソルが移動したらスワイプ処理を終
+      stage.addEventListener('pointerleave', onPointerup, false);
+    } else if ('ontouchstart' in window) {
+      stage.addEventListener('touchstart', onTouchstart, false);
+      stage.addEventListener('touchmove', onTouchmove, false);
+      stage.addEventListener('touchend', onTouchend, false);
+      // 何らかの原因でタッチが中断されたときにもタッチ終了時と同じ動作を実行
+      stage.addEventListener('touchcancel', onTouchend, false);
+    } else {
+      stage.addEventListener('mouseenter', onPointerenter, false);
+      stage.addEventListener('mouseleave', onPointerleave, false);
+    }
+    // クリックイベント
+    stage.addEventListener('click', onClick, false);
     // ボタンの挿入
     appendNavigations();
     // 一番最初のパネルを表示
